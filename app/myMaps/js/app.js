70dbe3a48be1f3669d65633f0497e3f0006eb2ab
Kevin Kelly <kevin.kelly@axiosengineering.com>
2015-03-09 15:04:18 -0400

    Updated myMaps for beter IWC/Open Layers separation

--- app/myMaps/js/app.js
+++ app/myMaps/js/app.js
@@ -1,138 +1,37 @@
-var map,popup;
-var featureId = 0;
-var locationData = {};
-var currentPopup = {};
-//create a vector source to add the icon(s) to.
-var vectorSource = new ol.source.Vector({});
-
-var vectorLayer = new ol.layer.Vector({
-    source: vectorSource
-});
-
-var addMarker = function(location,featId){
-    var coords = ol.proj.transform([location.coords.long, location.coords.lat], 'EPSG:4326', 'EPSG:3857');
-    for(var i in coords){
-        if(!coords[i]) {
-            return;
-        }
-    }
-
-    location.mapCoords = coords;
-    //create icon
-    var iconFeature = new ol.Feature({
-        geometry: new ol.geom.Point(coords),
-        name: location.title,
-        data: location
-    });
-    featId = featId || featureId++;
-    iconFeature.setId(featId);
-    locationData[featId] = location;
-    //add icon to vector source
-    map.getLayers().item(1).getSource().addFeatures([iconFeature]);
-    map.getView().setCenter(coords);
-
-    return featId;
-
-
-};
-
-function spawnPopup(coord,id,data){
-    console.log(data);
-    if(popup!=null) return;
-    currentPopup.coords = coord;
-    currentPopup.data= data;
-    currentPopup.id = id;
-    popup = $('<div class="popup"><b><p>' + data.title + '</p></b>' +
-    '<p>Lat/Long:'  +  data.coords.lat + ',' + data.coords.long +'</p></div>');
-
-    var overlay = new ol.Overlay({
-        element:popup
-    });
-
-    map.addOverlay(overlay);
-    overlay.setPosition(coord);
-}
-
-function destroyPopup(){
-    $(popup).remove();
-    popup = null;
-    currentPopup = {};
-}
-
 $(document).ready(function() {
-    map = new ol.Map({
-        target: 'map',
-        layers: [
-            new ol.layer.Tile({
-                source: new ol.source.MapQuest({layer: 'sat'})
-            }),
-            vectorLayer
-        ],
-        view: new ol.View({
-            center: [0, 0],
-            zoom: 3
-        })
-    });
+    var map = new CustomMap("map");
 
 
-    map.on('singleclick', function(evt){
-        var coord = evt.coordinate;
-        var feature = map.forEachFeatureAtPixel(evt.pixel,
-            function(feature, layer) {
-                return feature;
-            });
-
-        destroyPopup();
-        if(feature) {
-            var id = feature.getId();
-            spawnPopup(coord,id, locationData[id]);
-        }
-    });
-
-
+    //Create the IWC Client
+    //Once the IWC client is connected configure it to map features.
     var client=new ozpIwc.Client({peerUrl: window.OzoneConfig.iwcUrl});
     client.connect().then(function(){
 
-        var onChange = function(reply) {
-           if(reply.entity.newValue){
+        var intents = client.intents();
+        var data = client.data();
 
-           }
-        };
-
-
-        client.api('intents.api').register("/json/coord/map",function(event) {
+        var mappingIntent = function(event){
+            // This intent is expected to receive a data.api resource name to be used.
             var resource = event.entity;
-            client.api('data.api').get(resource).then(function(resp){
-                console.log(resp);
-                var id = addMarker(resp.entity,resource);
+
+            // Gather the resource
+            data.get(resource).then(function(resp){
+
+                // Attempt to add the resource to the map as a marker
+                var id = map.addMarker(resp.entity,resource);
+
+                // If the marker was successfully created, watch the resource for further changes
                 if(typeof id !== "undefined") {
-                    return client.api('data.api').watch(resource, function (event) {
-                        var feature = map.getLayers().item(1).getSource().getFeatureById(resource);
-                        map.getLayers().item(1).getSource().removeFeature(feature);
-
-                        //If this marker is currently popped up close it!
-                        var needsUpdate = false;
-                        if(currentPopup.id === resource){
-                            needsUpdate = true;
-                            destroyPopup();
-                        }
-
-                        if (event.entity.newValue) {
-                            addMarker(event.entity.newValue,resource);
-
-                            //Redraw the popup with the new data/location if needed
-                            if(needsUpdate){
-                                spawnPopup(locationData[resource].mapCoords,resource ,locationData[resource]);
-                            }
-                        }
+                    return data.watch(resource, function (event) {
+                        var newValue = event.entity.newValue;
+                        map.updateMarker(resource,newValue);
                     });
                 }
-            })['catch'](function(e){
-                console.error(e);
             });
-        });
+        };
+
+        intents.register("/json/coord/map",mappingIntent);
     });
-
-
-
 });
+
+
