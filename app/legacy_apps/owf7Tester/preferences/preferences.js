33e1940ef8135ac6a0940f1f38df2c59f72d5ec6
Alan Ward <arw180@gmail.com>
2015-05-14 13:38:04 -0400

    updates to findWidgets method

--- app/legacy_apps/owf7Tester/preferences/preferences.js
+++ app/legacy_apps/owf7Tester/preferences/preferences.js
@@ -6,4 +6,33 @@ angular.module('owf7Tester.preferences')
 	.controller('PreferencesCtrl', function($scope, $rootScope, $state, $log) {
 		$log.info('Preferences Ctrl');
 		$scope.pageName = 'Preferences';
+
+		$scope.callFindWidgets = function(form) {
+			$log.info('findWidgetsUserOnly: ' + $scope.findWidgetsUserOnly);
+			$log.info('findWidgetsWidgetVersion: ' + $scope.findWidgetsWidgetVersion);
+			$log.info('findWidgetsWidgetName: ' + $scope.findWidgetsWidgetName);
+			$log.info('findWidgetsWidgetGuid: ' + $scope.findWidgetsWidgetGuid);
+			$log.info('findWidgetsUniversalName: ' + $scope.findWidgetsUniversalName);
+			$log.info('findWidgetsGroupId: ' + $scope.findWidgetsGroupId);
+
+			OWF.Preferences.findWidgets({
+                    searchParams: {
+                        universalName: $scope.findWidgetsUniversalName
+                    },
+                    onSuccess: function (result) {
+                        var guid;
+                        if (result && result[0]) {
+                            guid = result[0].id;
+
+                           	$log.info('Found widget with guid: ' + guid);
+                        } else {
+                        	$log.warn('no widget found');
+                        }
+
+                    },
+                    onFailure: function (err) {
+                    	$log.error('Error invoking findWidgets: ' + err)
+                    }
+                });
+		};
 	});
