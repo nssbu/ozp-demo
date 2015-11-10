40538893edc3eabb38e9ecfaa1ce41321d21b8f5
Kevin Kelly <kevin.kelly@axiosengineering.com>
2015-11-10 13:46:29 -0500

    reverted bouncing balls data.api fps setting. For simplicity of example use fps on query param.

--- app/bouncingBalls/js/bouncingBalls.js
+++ app/bouncingBalls/js/bouncingBalls.js
@@ -3,10 +3,11 @@ bouncing.balls= bouncing.balls || {};
 bouncing.ourBalls = bouncing.ourBalls || [];
 bouncing.currentColor=bouncing.currentColor || "black";
 
+var params=ozpIwc.util.parseQueryParams();
+
 $(document).ready(function(){
     var colors=$("#ballColor option");
 
-    var params=ozpIwc.util.parseQueryParams();
 
     bouncing.currentColor=params['color'] || colors[Math.floor(Math.random() * colors.length)].value;
     $("#ballColor").val(bouncing.currentColor);
@@ -39,25 +40,10 @@ var client=new ozpIwc.Client({peerUrl: window.OzoneConfig.iwcUrl});
 client.connect().then(function(){
 	// setup
 	var viewPort=$('#viewport');
-    var fps= 20;
+    var fps = params['fps'] || 20;
     $('#myAddress').text(client.address);
     $('#fps').text(""+fps);
 
-
-    //=================================================================
-    // Set up framerate modification watching
-    var onFpsChange = function(response){
-        fps = response.entity.newValue || fps;
-    };
-
-    client.data().watch('/balls/framerate',fps).then(function(resp){
-        if(resp.entity) {
-            fps = resp.entity;
-        } else {
-            client.data().set("/balls/framerate",{entity: fps});
-        }
-        $('#fps').text(""+fps);
-    });
 
     //=================================================================
 	// cleanup when we are done
